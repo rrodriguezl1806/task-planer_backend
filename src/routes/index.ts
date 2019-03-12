d261f32690a89fb66545ce7184c10b7ebe9bca4b
Reynaldo R.L <reynaldo@generalsoftwareinc.com>
2019-03-12 14:14:47 -0400

    update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,15 @@
-export { TaskRoutes } from "./task"
+// Routes
+import { TaskRoutes } from "./task";
+
+export class Routes {
+
+    private taskRoutes: TaskRoutes
+
+    constructor() {
+        this.taskRoutes = new TaskRoutes()
+    }
+
+    public routes(app: any): void {    
+        this.taskRoutes.routes(app);
+    }
+ }
--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,30 +1,24 @@
-import * as express from "express";
-
 // Controllers
 import { TaskController } from '../controllers/tasks.controller'
 
 export class TaskRoutes {
 
-    private tasksCtrl: TaskController  = new TaskController()
+    private tasksCtrl: TaskController
+    private url = "/api/task"
 
-    public routes(api: express.Application): void {
+    constructor() {
+        this.tasksCtrl = new TaskController()
+    }
+
+    public routes(api: any): void {
 
         // Add new task
-        api.post('/api/addNewTask', this.tasksCtrl.addNewTask)
+        api.post(`${this.url}/addNewTask`, this.tasksCtrl.addNewTask)
         
-        // Get all tasks
-        api.get('/api/getTasks', this.tasksCtrl.getTasks)
+        // // Get all tasks
+        api.get(`${this.url}/getTasks`, this.tasksCtrl.getTasks)
         
-        // Get task by Id
-        api.get('/api/getTaskById/:id', this.tasksCtrl.getTaskById)
-        
-        // // Eliminar
-        // router.delete('/product/:id', ProductsCtrl.deleteProduct)
-        // // Update
-        // router.update('/product/:id', ProductsCtrl.updateProduct)
-        // // SignUp
-        // router.post('/signUp', userCtrl.signUp)
-        // // SignIn
-        // router.post('/signIn', userCtrl.signIn)
+        // // Get task by Id
+        api.get(`${this.url}/getTaskById/:id`, this.tasksCtrl.getTaskById)
     }
- }
+}
